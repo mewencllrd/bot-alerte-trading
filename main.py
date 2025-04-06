90bdaf1046675b884d9a832a8a7c2bdb5a85d2e5
mewencllrd <mewencallard@gmail.com>
2025-04-06 20:04:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def fetch_price(symbol):
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     data = {
-        "chat_id": CHAT_ID,
+        "chat_id": TELEGRAM_CHAT_ID,
         "text": message,
         "parse_mode": "Markdown"
     }
