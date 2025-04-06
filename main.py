bf591a8531f0037a2d8a7744907e3b82348d62fb
mewencllrd <mewencallard@gmail.com>
2025-04-06 20:24:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import threading
 
 # === CONFIGURATION ===
 TELEGRAM_TOKEN = "TON_TOKEN"
-TELEGRAM_CHAT_ID = -1002516223605
+CHAT_ID = "-1002516223605"
 SYMBOLS = ["BTC/USDT", "ETH/USDT"]
 SCAN_INTERVAL = 60
 
@@ -35,7 +35,7 @@ def fetch_price(symbol):
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     data = {
-        "chat_id": TELEGRAM_CHAT_ID,
+"chat_id": CHAT_ID,
         "text": message,
         "parse_mode": "Markdown"
     }
