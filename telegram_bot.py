a68e3593610e2112b8ba866d59efa97da621bf2c
mewencllrd <mewencallard@gmail.com>
2025-04-12 11:14:25 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,9 @@
 def start_bot():
-    print("🤖 Bot Telegram en cours de démarrage...")
+    print("🤖 Bot Telegram en cours de démarrage... ")
 
     # Appelle ici les fonctions du bot que tu veux lancer
     # Exemple :
     from price_tracker import launch_price_check_loop
     launch_price_check_loop()
 
-    # Tu peux aussi lancer d'autres tâches comme l'envoi automatique de récap
+    # Tu peux aussi lancer d’autres tâches comme l’envoi automatique de récap
