4580c734e306420a0151e1ccdbb6303ccd15dd20
mewencllrd <mewencallard@gmail.com>
2025-04-12 11:02:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("✅ Bot lancé sur Railway")
 from indicators import *
 # ... (main bot fusionné ici)
+if __name__ == "__main__":
+    print("👀 Initialisation du bot en cours...")
+
+    from telegram_bot import start_bot
+    start_bot()
